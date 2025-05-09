9c7a4a0cddd168fafe2c8b22c86a9ab23dad8458
peraag3 <164960208+peraag3@users.noreply.github.com>
2025-05-09 16:39:45 -0300

    Update db.js

--- db.js
+++ db.js
@@ -16,3 +16,23 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
+
+// backend/db.js
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // seu usuário do MySQL
+    password: '', // sua senha do MySQL
+    database: 'agenda_de_contatos' // nome do banco de dados
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conectado ao banco de dados MySQL!');
+});
+
+module.exports = db;
